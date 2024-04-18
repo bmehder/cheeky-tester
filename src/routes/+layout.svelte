8dee104f00d39032d813bfb646a0e96fdf544785
bmehder <bmehder@gmail.com>
2024-04-18 03:54:03 +0100

    first

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import '$lib/app.css'
+</script>
+
+<main class="container">
+	<slot />
+</main>
+
+<style>
+	main {
+		display: grid;
+		gap: 3rem;
+		padding-block: 3rem;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Job from '$lib/Job.svelte'
+
+	export let data
+</script>
+
+{#each data.jobsByRegions as region}
+	<div class="flow">
+		<h3>{region.state}</h3>
+		<div class="columns">
+			{#each region.entries as job}
+				<Job {job} />
+			{/each}
+		</div>
+	</div>
+{/each}
