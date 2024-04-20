9f83d23cc8514e46e7609f127c4cf45867e242fe
bmehder <bmehder@gmail.com>
2024-04-20 17:36:33 +0100

    error page

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,12 +1,23 @@
+import { error } from '@sveltejs/kit'
+
 export const ssr = false
 
 export async function load({ fetch }) {
 	const route = 'https://cheekycms.com/wp-json/bsm/v1/jobs'
+	// const params = '?arrange=regions&arrangeby=states'
 	const params = '?arrange=regions&arrangeby=countries'
 
 	const response = await fetch(route + params)
+	console.log(response)
+
+	if (!response.ok) {
+		error(404, {
+			message: 'Response is not okay.',
+		})
+	}
+
 	const jobsByRegions = await response.json()
-	
+
 	return {
 		jobsByRegions,
 	}
