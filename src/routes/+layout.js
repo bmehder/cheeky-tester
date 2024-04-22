e9a9a0483e76c8becc163190d36113057cb5f999
bmehder <bmehder@gmail.com>
2024-04-22 18:35:59 +0100

    don't trust last deployment

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,16 +1,17 @@
+export const prerender = true
 export const trailingSlash = 'always'
 
 export async function load({ fetch }) {
 	const zones = await fetch(`https://cheekycms.com/wp-json/wp/v2/zones`)
 		.then(x => x.json())
 		.catch(console.error)
-	
+
 	const teams = await fetch(`https://cheekycms.com/wp-json/wp/v2/teams`)
 		.then(x => x.json())
 		.catch(console.error)
 
 	return {
 		zones,
-		teams
+		teams,
 	}
 }
